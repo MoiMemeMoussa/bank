735a4d4d6a0e776bf3bee58df1ae2dfdfdc12411
Moussa FALL <moussafall005@gmail.com>
2019-08-16 22:03:17 +0200

    committing directory src and file pom.xml

---
+++ src/main/java/sn/oneclic/bank/banksn/BanksnApplication.java
@@ -0,0 +1,13 @@
+package sn.oneclic.bank.banksn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksnApplication.class, args);
+	}
+
+}
